b132485cad0d8bd783b288721e71be31103aa31a
myrows <myrows.contactme@gmail.com>
2020-06-03 21:37:33 +0200

    Changes in map

--- Covirap-app/Covirapp/app/src/main/java/com/example/covirapp/ui/radar/MapActivity.kt
+++ Covirap-app/Covirapp/app/src/main/java/com/example/covirapp/ui/radar/MapActivity.kt
@@ -10,42 +10,29 @@ import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.airbnb.lottie.LottieCompositionFactory
-import com.airbnb.lottie.LottieDrawable
 import com.example.covirapp.MainActivity
 import com.example.covirapp.R
 import com.example.covirapp.api.CovirappService
 import com.example.covirapp.api.generator.ServiceGenerator
 import com.example.covirapp.common.SharedPreferencesManager
 import com.example.covirapp.models.Ubicacion
-import com.example.covirapp.models.UsersResponse
-import com.example.covirapp.models.UsersResponseItem
-import com.example.covirapp.ui.account.DetectObjectActivity
-import com.example.covirapp.ui.account.MyAccountActivity
-import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationListener
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapFragment
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_map.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     lateinit var supportMapFragment: SupportMapFragment
     lateinit var client: FusedLocationProviderClient
@@ -71,14 +58,11 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
         userStats = ""
 
         // Instances Firebase y Location
-        locationManager =
-            getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
         db = FirebaseFirestore.getInstance()
         supportMapFragment =
             supportFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment
         supportMapFragment.getMapAsync(this)
-        client = LocationServices.getFusedLocationProviderClient(this)
+        //client = LocationServices.getFusedLocationProviderClient(this)
 
         // Data of SharedPreferences
 
@@ -86,35 +70,9 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
         mascarillaFound = SharedPreferencesManager.SharedPreferencesManager.getSomeBooleanValue("mascarillaFound")
         userStats = SharedPreferencesManager.SharedPreferencesManager.getSomeStringValue("status").toString()
 
-        // Check permissions
+        // Get user ubication and add marker
 
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-
-            // Check mask
-            if ( !mascarillaFound ) alertNotMask()
-
-            ubication = LocationServices.getFusedLocationProviderClient(this)
-            ubication.lastLocation.addOnSuccessListener {
-
-                if (it != null) {
-                    giveMeYourUbication(it)
-                }
-                addMarkers()
-            }
-        } else {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ),
-                1
-            )
-        }
+        giveMeYourUbication(getLocation()!!)
 
         floatingActionButtonRadar.setOnClickListener {
             addMarkers()
@@ -126,8 +84,9 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
     override fun onMapReady(googleMap: GoogleMap?) {
         mMap = googleMap!!
 
-        mMap.clear()
+        if( !mascarillaFound ) alertNotMask()
 
+        mMap.clear()
     }
 
     fun giveMeYourUbication(location: Location) {
@@ -201,10 +160,12 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
                     Log.w("Query", "Error getting documents: ", exception)
                 }
         }
+        addMarkers()
+
     }
 
     fun addMarkers () {
-        mMap.clear()
+
         supportMapFragment.getMapAsync {
 
             db.collection("ubicaciones")
@@ -242,45 +203,6 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
         }
     }
 
-    override fun onLocationChanged(newLocation: Location?) {
-
-        db.collection("ubicaciones")
-            .whereEqualTo("userId", userId)
-            .get()
-            .addOnSuccessListener { documents ->
-                for (document in documents) {
-                    Log.d("Query", "${document.id} => ${document.data}")
-
-                    if (documents.size() > 0) {
-
-                        var map = mutableMapOf<String, Any>()
-                        map.put(key = "latitud", value = newLocation!!.latitude)
-                        map.put(key = "longitud", value = newLocation!!.longitude)
-                        map.put(key = "mascarilla", value = mascarillaFound)
-                        map.put(key = "userStatus", value = userStats)
-                        map.put(key = "userId", value = userId)
-                        db.collection("ubicaciones").document(document.id).update(map)
-                            .addOnSuccessListener {
-                                Log.d(
-                                    "Update",
-                                    "Se ha actualizado los datos correctamente"
-                                )
-                                addMarkers()
-
-                            }.addOnFailureListener {
-                                Log.d(
-                                    "Update",
-                                    "Ha ocurrido un error al actualizar el documento"
-                                )
-                            }
-                    }
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.w("Query", "Error getting documents: ", exception)
-            }
-    }
-
     private fun alertNotMask () {
         val dialog  = AlertDialog.Builder(this)
         val dialogView = layoutInflater.inflate(R.layout.custom_object_map_error, null)
@@ -293,5 +215,56 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
             var intentDetector = Intent( this@MapActivity, MainActivity::class.java )
             startActivity( intentDetector )
         }
+    }
+
+    fun getLocation(): Location? {
+        var finalLoc: Location? = null
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                try {
+                    var gps_enabled = false
+                    var network_enabled = false
+
+                    val lm : LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+                    gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                    network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+                    var net_loc: Location? = null
+                    var gps_loc: Location? = null
+
+                    if (gps_enabled) gps_loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                    if (network_enabled) net_loc = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+
+                    if (gps_loc != null && net_loc != null) {
+
+                        //smaller the number more accurate result will
+                        finalLoc = if (gps_loc.accuracy > net_loc.accuracy) net_loc else gps_loc
+
+                        // I used this just to get an idea (if both avail, its upto you which you want to take as I've taken location with more accuracy)
+                    } else {
+                        if (gps_loc != null) {
+                            finalLoc = gps_loc
+                        } else if (net_loc != null) {
+                            finalLoc = net_loc
+                        }
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ),
+                    1
+                )
+            }
+
+        return finalLoc
     }
 }
