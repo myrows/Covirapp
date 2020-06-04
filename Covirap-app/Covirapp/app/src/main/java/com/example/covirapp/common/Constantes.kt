a99a8df19acbf4ad1ea777dab73e50bdb555974c
myrows <myrows.contactme@gmail.com>
2020-06-04 13:00:21 +0200

    Covirapp 2.0

--- Covirap-app/Covirapp/app/src/main/java/com/example/covirapp/common/Constantes.kt
+++ Covirap-app/Covirapp/app/src/main/java/com/example/covirapp/common/Constantes.kt
@@ -3,6 +3,7 @@ package com.example.covirapp.common
 class Constantes {
 
     companion object {
+        //const val API_BASE_URL = "http://10.0.2.2:9000"
         const val API_BASE_URL = "http://ec2-15-188-232-141.eu-west-3.compute.amazonaws.com:9000"
         const val API_BASE_URL_COVID_COUNTRY = "https://disease.sh/v2/"
         const val API_BASE_URL_REGIONS = "https://api.covid19tracking.narrativa.com"
